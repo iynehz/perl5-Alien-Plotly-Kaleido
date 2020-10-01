f7893216155a8f8754c5ac4f9eb0456cf589ce6e
Stephan Loyd <stephanloyd9@gmail.com>
2020-10-02 00:06:03 +0800

    fix windows

--- util/MyInstallUtil.pm
+++ util/MyInstallUtil.pm
@@ -24,18 +24,30 @@ sub detect_kaleido_version {
         my $data;
         eval {
             my ( $in, $out, $err );
-            $h =
-              IPC::Run::start( $cmd, \$in, '>pty>', \$out, '2>', \$err,
-                my $t = IPC::Run::timer(20) );
+            my $t = IPC::Run::timer(30);
+            if ( $^O eq 'MSWin32' ) {
+                $h = IPC::Run::start( $cmd, \$in, \$out, \$err, $t );
+            }
+            else {
+                $h =
+                  IPC::Run::start( $cmd, \$in, '>pty>', \$out, '2>', \$err,
+                    $t );
+            }
             while ( not $data and not $t->is_expired ) {
                 $h->pump;
                 $data = $decode_json_safe->($out);
             }
+            if ($^O eq 'MSWin32') {
+                $h->finish;
+            }
         };
+        if ($^O ne 'MSWin32') {
+            $h->kill_kill;
+        }
         if ($@) {
             warn $@;
+            $h->kill_kill;
         }
-        $h->kill_kill;
         if ( $data and exists $data->{version} ) {
             return $data->{version};
         }
