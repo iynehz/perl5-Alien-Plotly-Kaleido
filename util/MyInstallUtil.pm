33ec0e6c11d3f3c16c8d1ca818959d7b3bad2de0
Stephan Loyd <stephanloyd9@gmail.com>
2020-10-02 00:06:03 +0800

    no need pty

--- util/MyInstallUtil.pm
+++ util/MyInstallUtil.pm
@@ -18,24 +18,32 @@ sub detect_kaleido_version {
             return $data;
         };
 
-        my $cmd = [ $kaleido, 'plotly', '--disable-gpu' ];
-
+        my @cmd = ( $kaleido, 'plotly', '--disable-gpu' );
+        eval {
+            require Chart::Plotly;
+            my $plotlyjs =
+              File::ShareDir::dist_file( 'Chart-Plotly',
+                'plotly.js/plotly.min.js' );
+              if ( -r $plotlyjs ) {
+                push @cmd, "--plotlyjs=$plotlyjs";
+            }
+        };
         my $h;
         my $data;
         eval {
             my ( $in, $out, $err );
-            $h =
-              IPC::Run::start( $cmd, \$in, '>pty>', \$out, '2>', \$err,
-                my $t = IPC::Run::timer(20) );
+            $h = IPC::Run::start( \@cmd, \$in, \$out, \$err,
+                my $t = IPC::Run::timer(30) );
             while ( not $data and not $t->is_expired ) {
                 $h->pump;
                 $data = $decode_json_safe->($out);
             }
+            $h->finish;
         };
         if ($@) {
             warn $@;
+            $h->kill_kill;
         }
-        $h->kill_kill;
         if ( $data and exists $data->{version} ) {
             return $data->{version};
         }
